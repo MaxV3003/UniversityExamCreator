0f206ec4cd92fbec5f150efe99a2dffea56c635f
MaxV3003 <165780517+MaxV3003@users.noreply.github.com>
2024-05-14 19:02:38 +0200

    Verzeichniserstellung

---
+++ UniversityExamCreator/Views/ExamPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UniversityExamCreator.View
+{
+    /// <summary>
+    /// Interaktionslogik für ExamPage.xaml
+    /// </summary>
+    public partial class ExamPage : Window
+    {
+        public ExamPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
