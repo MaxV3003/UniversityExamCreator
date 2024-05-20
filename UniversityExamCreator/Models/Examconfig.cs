6f181d723c451ef23b5d4208dcc441afb07a4294
MaxV3003 <165780517+MaxV3003@users.noreply.github.com>
2024-05-20 15:33:10 +0200

    Added Examconfig Content. Refactored the AskpropertyItem in Task.cs

--- UniversityExamCreator/Models/Examconfig.cs
+++ UniversityExamCreator/Models/Examconfig.cs
@@ -8,7 +8,71 @@ namespace UniversityExamCreator.Models
 {
     internal class Examconfig
     {
-        public string ExamName { get; set; }
+        /// <summary>
+        /// Name / Identifire of the Examconfig-Element.
+        /// </summary>
+        public string ConfigName { get; set; }
 
+        /// <summary>
+        /// Identifire of the Module. 
+        /// </summary>
+        public string ModuleID {  get; set; }
+
+        /// <summary>
+        /// Typ of the Exam. "Einheitliche Form" or "Mischform".
+        /// </summary>
+        public string ExamTyp { get; set; }
+
+        /// <summary>
+        /// Amount of Tasks which should be included in the Exam.
+        /// </summary>
+        public int TaskAmount { get; set; }
+
+        /// <summary>
+        /// Amount of Points the Exam contains. 
+        /// </summary>
+        public float PointAmount {  get; set; }
+
+        /// <summary>
+        /// List of Taskgroups the Exam contains. 
+        /// </summary>
+        public List<Taskgroupe> TaskGroups { get; set; }
+
+        /// <summary>
+        /// Constructor for an Examconfig-Element.
+        /// </summary>
+        public Examconfig(string examName, string moduleID, string examTyp) 
+        {
+            ConfigName = examName;
+            ModuleID = moduleID;
+            ExamTyp = examTyp;
+            TaskAmount = 0;
+            PointAmount = 0;
+            TaskGroups = new List<Taskgroupe>();
+        }
+
+        /// <summary>
+        /// Methode to set the Amount of Tasks an Exam should contain.
+        /// </summary>
+        public void setTaskAmount(int taskAmount)
+        {
+            TaskAmount = taskAmount;
+        }
+
+        /// <summary>
+        /// Methode to set the Amount of Points an Exam shoukd contain.
+        /// </summary>
+        public void setPointAmount(int pointAmount) 
+        { 
+            PointAmount = pointAmount;   
+        }
+
+        /// <summary>
+        /// Taskgroupe the Exam should contain. 
+        /// </summary>
+        public void addTaskGroupe(Taskgroupe taskGroupe) 
+        {
+            TaskGroups.Add(taskGroupe);
+        }
     }
 }
