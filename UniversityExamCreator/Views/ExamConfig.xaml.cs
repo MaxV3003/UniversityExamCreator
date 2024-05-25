a7db2e587b1630a250c916d88d8aff28a0466675
MaxV3003 <165780517+MaxV3003@users.noreply.github.com>
2024-05-25 16:13:07 +0200

    Deleted all Windows from Views and added first Page KlausurConfig

--- UniversityExamCreator/Views/ExamConfig.xaml.cs
+++ UniversityExamCreator/Views/ExamConfig.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace UniversityExamCreator.Views
 {
     /// <summary>
-    /// Interaktionslogik für TaskPage.xaml
+    /// Interaktionslogik für ExamConfig.xaml
     /// </summary>
-    public partial class TaskPage : Window
+    public partial class ExamConfig : Page
     {
-        public TaskPage()
+        public ExamConfig()
         {
             InitializeComponent();
         }
