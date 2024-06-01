975845906033fe98611a19813f7e5a9d90f4d0a8
MaxV3003 <165780517+MaxV3003@users.noreply.github.com>
2024-06-01 15:34:29 +0200

    Finished the first step of ExamCreate-Page.

--- UniversityExamCreator/Views/ExamCreate.xaml.cs
+++ UniversityExamCreator/Views/ExamCreate.xaml.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UniversityExamCreator.Views
 {
@@ -23,6 +14,17 @@ namespace UniversityExamCreator.Views
     {
         // ObservableCollection to hold the items
         public ObservableCollection<Item> Items { get; set; }
+        public ObservableCollection<Item> SelectedItems { get; set; }
+        public ObservableCollection<Item> FilteredItems { get; set; }
+
+        //Lists to save the the Filteropotion-Items 
+        public List<string> Themes { get; set; }
+        public List<int> Points { get; set; }
+        public List<string> Difficulties { get; set; }
+        public string SelectedTheme { get; set; }
+        public int SelectedPoints { get; set; }
+        public string SelectedDifficulty { get; set; }
+
 
         public ExamCreate()
         {
@@ -31,43 +33,78 @@ namespace UniversityExamCreator.Views
             // Initialize the ObservableCollection and add items
             Items = new ObservableCollection<Item>
             {
-                new Item { Name = "Item is cool but does it have much place to exist1", IsSelected = false, Info = "Information about Item 1" },
-                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2" },
-                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3" },
-                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1" },
-                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2" },
-                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3" },
-                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1" },
-                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2" },
-                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3" },
-                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1" },
-                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2" },
-                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3" },
-                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1" },
-                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2" },
-                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3" },
-                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1" },
-                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2" },
-                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3" },
-                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1" },
-                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2" },
-                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3" },
+                new Item { Name = "Item is cool but does it have much place to exist1", IsSelected = false, Info = "Information about Item 1", Points=2, Theme ="Test", Difficulty="leicht"},
+                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2", Points = 2, Theme="Datenbanken", Difficulty="leicht"},
+                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3", Points = 3, Theme="Datenbanken", Difficulty="schwer"},
+                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1", Points = 3, Theme="Programmieren", Difficulty="normal"},
+                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2", Points = 1, Theme="Programmieren", Difficulty="schwer"},
+                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3", Points = 1, Theme="Lineare Algebra", Difficulty="schwer"},
+                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1", Points = 3, Theme="Lineare Algebra", Difficulty="normal"},
+                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2", Points = 2, Theme="Datenbanken", Difficulty="leicht"},
+                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3", Points = 2, Theme="Programmieren", Difficulty="leicht"},
+                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1", Points = 3, Theme="Lineare Algebra", Difficulty="leicht"},
+                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2", Points = 2, Theme="Datenbanken", Difficulty="normal"},
+                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3", Points = 2, Theme="Datenbanken", Difficulty="normal"},
+                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1", Points = 1, Theme="Datenbanken", Difficulty="normal"},
+                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2", Points = 1, Theme="Datenbanken", Difficulty="schwer"},
+                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3", Points = 1, Theme="Datenbanken", Difficulty="leicht"},
+                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1", Points = 2, Theme="Datenbanken", Difficulty="leicht"},
+                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2", Points = 2, Theme="Datenbanken", Difficulty="leicht"},
+                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3", Points = 3, Theme="Datenbanken", Difficulty="leicht"},
+                new Item { Name = "Item 1", IsSelected = false, Info = "Information about Item 1", Points = 2, Theme="Datenbanken", Difficulty="leicht"},
+                new Item { Name = "Item 2", IsSelected = false, Info = "Information about Item 2", Points = 2, Theme="Datenbanken", Difficulty="leicht"},
+                new Item { Name = "Item 3", IsSelected = false, Info = "Information about Item 3", Points = 1, Theme="Datenbanken", Difficulty="leicht"},
             };
+
+            // Initialize the filtered items collection
+            FilteredItems = new ObservableCollection<Item>();
+            SelectedItems = new ObservableCollection<Item>();
+
+            // Initialize the themes list and populate it with distinct themes
+            Themes = Items.Select(i => i.Theme).Distinct().ToList();
+            // Add default option to Themes
+            Themes.Insert(0, "Alle Themen");
+            // Set default selected theme
+            SelectedTheme = Themes[0];
+
+            // Initialize the points list and populate it with distinct points
+            Points = Items.Select(i => i.Points).Distinct().ToList();
+            // Add default option to Points
+            Points.Insert(0, 0);
+            // Set default selected points
+            SelectedPoints = Points[0];
+
+            Difficulties = Items.Select(i => i.Difficulty).Distinct().ToList();
+            Difficulties.Insert(0, "Alle Schwierigkeiten");
+            SelectedDifficulty = Difficulties[0];
 
             // Set the DataContext to the current instance
             DataContext = this;
+
+            // Apply initial filters
+            ApplyFilters();
+
         }
 
+        /// <summary>
+        /// Button to get to the next Page.
+        /// </summary>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new ExamPreview());
         }
 
+        /// <summary>
+        /// Button to get to the previous Page.
+        /// </summary>
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new ExamConfig());
         }
 
+        /// <summary>
+        /// Button to get the Info of an Item.
+        /// </summary>
         private void InfoButton_Click(object sender, RoutedEventArgs e)
         {
             // Get the item from the Tag property
@@ -78,13 +115,86 @@ namespace UniversityExamCreator.Views
             }
         }
 
+        /// <summary>
+        /// Dropdown-Changes will be initialized after selecting a new Filteroption. 
+        /// </summary>
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Filter the items based on the selected theme and points
+            ApplyFilters();
+        }
+
+        /// <summary>
+        /// Methode to swap the Items in the left Box.
+        /// </summary>
+        private void ApplyFilters()
+        {
+            // Apply theme filter
+            var filteredByTheme = Items.Where(i => SelectedTheme == null || SelectedTheme == "Alle Themen" || i.Theme == SelectedTheme);
+            SelectedItems.Clear();
+            foreach (var item in Items.Where(i => i.IsSelected))
+            {
+                SelectedItems.Add(item);
+            }
+
+            // Apply points filter
+            var filteredByPoints = SelectedPoints == 0 ? filteredByTheme : filteredByTheme.Where(i => i.Points == SelectedPoints);
+
+            // Update FilteredItems
+            FilteredItems.Clear();
+            foreach (var item in filteredByPoints)
+            {
+                FilteredItems.Add(item);
+            }
+
+            // Apply difficulty filter
+            var filteredByDifficulty = SelectedDifficulty == null || SelectedDifficulty == "Alle Schwierigkeiten"
+                ? filteredByPoints
+                : filteredByPoints.Where(i => i.Difficulty == SelectedDifficulty);
+
+            // Update FilteredItems
+            FilteredItems.Clear();
+            foreach (var item in filteredByDifficulty)
+            {
+                FilteredItems.Add(item);
+            }
+        }
+
+
+        /// <summary>
+        /// Button to add the Items that should be in the Exam.
+        /// </summary>
+        private void AddSelectedItemsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedItems.Clear();
+            foreach (var item in Items.Where(i => i.IsSelected))
+            {
+                SelectedItems.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Button to delete the Items from the List of the Items which sould be in the Exam.
+        /// </summary>
+        private void DeleteSelectedItemsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var itemsToRemove = SelectedItems.Where(i => i.IsSelectedForDeletion).ToList();
+            foreach (var item in itemsToRemove)
+            {
+                SelectedItems.Remove(item);
+            }
+        }
 
         // Item class
         public class Item
         {
             public string Name { get; set; }
             public bool IsSelected { get; set; }
+            public bool IsSelectedForDeletion { get; set; }
             public string Info { get; set; }
+            public int Points { get; set; }
+            public string Theme { get; set; }
+            public string Difficulty { get; set; }
         }
     }
 }
