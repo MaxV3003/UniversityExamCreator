81c1f935e5d7b4593f8b35b62795d22e094139a7
Mannymarc02 <marc-jordan@gmx.de>
2024-06-02 14:22:06 +0200

    Datenbanken

---
+++ UniversityExamCreator/Models/Datenbanken.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+using System;
+using System.Data.SQLite;
+
+public class DatabaseManager
+{
+    private string connectionString;
+
+    public DatabaseManager(string databasePath)
+    {
+        connectionString = $"Data Source={databasePath};Version=3;";
+    }
+
+    public void CreateTables()
+    {
+        using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+        {
+            conn.Open();
+
+            string createAufgabeTable = @"
+                CREATE TABLE IF NOT EXISTS aufgabe (
+                    id INTEGER PRIMARY KEY,
+                    task_content TEXT NOT NULL,
+                    points INTEGER NOT NULL,
+                    difficulty VARCHAR(50) NOT NULL,
+                    subject VARCHAR(100) NOT NULL,
+                    date_created DATE NOT NULL,
+                    author VARCHAR(100) NOT NULL
+                );";
+
+            string createKlausurTable = @"
+                CREATE TABLE IF NOT EXISTS klausur (
+                    id INTEGER PRIMARY KEY,
+                    course VARCHAR(100) NOT NULL,
+                    examiner VARCHAR(100) NOT NULL,
+                    date DATE NOT NULL
+                );";
+
+            string createKlausurAufgabeTable = @"
+                CREATE TABLE IF NOT EXISTS klausur_aufgabe (
+                    id INTEGER PRIMARY KEY,
+                    klausur_id INTEGER NOT NULL,
+                    aufgabe_id INTEGER NOT NULL,
+                    FOREIGN KEY (klausur_id) REFERENCES klausur(id),
+                    FOREIGN KEY (aufgabe_id) REFERENCES aufgabe(id)
+                );";
+
+            string createNutzerTable = @"
+                CREATE TABLE IF NOT EXISTS nutzer (
+                    id INTEGER PRIMARY KEY,
+                    username VARCHAR(50) NOT NULL,
+                    password VARCHAR(20) NOT NULL
+                );";
+
+            string createAntwortTable = @"
+                CREATE TABLE IF NOT EXISTS antwort (
+                    id INTEGER PRIMARY KEY,
+                    aufgabe_id INTEGER NOT NULL,
+                    answer_content TEXT NOT NULL,
+                    username VARCHAR(50) NOT NULL,
+                    FOREIGN KEY (aufgabe_id) REFERENCES aufgabe(id)
+                );";
+
+            ExecuteNonQuery(conn, createAufgabeTable);
+            ExecuteNonQuery(conn, createKlausurTable);
+            ExecuteNonQuery(conn, createKlausurAufgabeTable);
+            ExecuteNonQuery(conn, createNutzerTable);
+            ExecuteNonQuery(conn, createAntwortTable);
+        }
+    }
+    private void ExecuteNonQuery(SQLiteConnection conn, string sql)
+    {
+        using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+        {
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
+
+
