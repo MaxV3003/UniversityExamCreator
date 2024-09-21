58d09b14cb51b911531706b17485782d64093fde
MartenD8 <marten.duerre@googlemail.com>
2024-09-21 18:33:17 +0200

    Felder können hinzugefügt werden

--- UniversityExamCreator/Views/TaskCreateMC.xaml.cs
+++ UniversityExamCreator/Views/TaskCreateMC.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -21,10 +23,29 @@ namespace UniversityExamCreator.Views
     public partial class TaskCreateMC : Page
     {
         Models.Task task;
+        private int textBoxCount = 0;
+        private double currentY = 10;
         public TaskCreateMC(Models.Task task)
         {
             InitializeComponent();
             this.task = task;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            
+            System.Windows.Controls.TextBox newTextBox = new System.Windows.Controls.TextBox();
+            newTextBox.Width = 490; 
+            newTextBox.Height = 60;
+            newTextBox.Margin = new Thickness(0, 10, 0, 0); 
+            Canvas.SetLeft(newTextBox, 100); 
+            Canvas.SetTop(newTextBox, currentY); 
+
+            currentY += 20; 
+
+            TextBoxContainer.Children.Add(newTextBox);
+
+            textBoxCount++;
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
